7244081df70ea0e66dcd67c010e817d90ec11bdf
mwkho <martin.h@hotmail.ca>
2021-03-01 22:53:23 +0000

    completed head function

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  let equality =  '!==';
+  let status = 'Failed';
+  if (actual === expected) {
+    equality = '===';
+    status = 'Passed';
+  }
+  console.log(`Assertion ${status}: ${actual} ${equality} ${expected}.`);
+};
+
+const head = function(arr1) {
+  return arr1[0];
+};
