75222f585cf08ccfe084b6e092f894572d378dbc
mwkho <martin.h@hotmail.ca>
2021-03-03 22:23:23 +0000

    changed the file name from Letter to Letter(s)

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  let equality =  '!==';
+  let status = 'Failed';
+  if (actual === expected) {
+    equality = '===';
+    status = 'Passed';
+  }
+  console.log(`Assertion ${status}: ${actual} ${equality} ${expected}.`);
+};
+
+const countLetters = function(myStr){
+  let result = {};
+  for (let character of myStr){
+    if (checkIsLetter(character)){ // filters out the nonletters
+      result[character] = !result[character] ? 1 : result[character] + 1;
+    }
+  }
+  return result;
+}
+
+const checkIsLetter = function(letter){
+  let abc = "abcdefghijklmnopqrstuvwxyz";
+  abc += abc.toUpperCase();
+  return (abc.indexOf(letter) !== -1);
+}
+
+/*
+let myString = 'this is a Loo0ng string of various character!';
+let result = countLetters(myString);
+
+assertEqual(result['L'], 1);
+assertEqual(result['!'], undefined);
+assertEqual(result[' '], undefined);
+assertEqual(result[''], undefined);
+assertEqual(result['0'], undefined);
+assertEqual(result['o'], 4);
+*/
