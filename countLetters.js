d7767d152aaff0bc2c9ed987bb4dc28a4b3cf53c
mwkho <martin.h@hotmail.ca>
2021-03-03 23:00:30 +0000

    linted countLetters

--- countLetters.js
+++ countLetters.js
@@ -8,21 +8,21 @@ const assertEqual = function(actual, expected) {
   console.log(`Assertion ${status}: ${actual} ${equality} ${expected}.`);
 };
 
-const countLetters = function(myStr){
+const countLetters = function(myStr) {
   let result = {};
-  for (let character of myStr){
-    if (checkIsLetter(character)){ // filters out the nonletters
+  for (let character of myStr) {
+    if (checkIsLetter(character)) { // filters out the nonletters
       result[character] = !result[character] ? 1 : result[character] + 1;
     }
   }
   return result;
-}
+};
 
-const checkIsLetter = function(letter){
+const checkIsLetter = function(letter) {
   let abc = "abcdefghijklmnopqrstuvwxyz";
   abc += abc.toUpperCase();
   return (abc.indexOf(letter) !== -1);
-}
+};
 
 /*
 let myString = 'this is a Loo0ng string of various character!';
