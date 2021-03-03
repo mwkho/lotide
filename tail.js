a7f5b2b562b8e79eb1a32b314df8e4a18eb685ea
mwkho <martin.h@hotmail.ca>
2021-03-03 02:31:00 +0000

    refactored tail.js function to be more simple and easy to read

--- tail.js
+++ tail.js
@@ -12,6 +12,6 @@ let tail = function(arr) {
   return arr.slice(1);
 };
 
- const words = ["Yo Yo", "Lighthouse", "Labs"];
- tail(words); // no need to capture the return value since we are not checking it
- assertEqual(words.length, 3); // original array should still have 3 elements!
+//  const words = ["Yo Yo"]//, "Lighthouse", "Labs"];
+//  tail(words); // no need to capture the return value since we are not checking it
+//  assertEqual(words.length, 1); // original array should still have 3 elements!
