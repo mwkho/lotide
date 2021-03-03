9cb200163823002b790af9a657a56c9661fab0b5
mwkho <martin.h@hotmail.ca>
2021-03-03 21:30:57 +0000

    completed countOnly function

--- countOnly.js
+++ countOnly.js
@@ -10,10 +10,15 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
+  for (const item of allItems){
+    if(itemsToCount[item]){
+      results[item] = results[item] ? results[item] + 1 : 1;
+    }
+  }
   return results; 
 };
 
-
+/*
 const firstNames = [
   'Karl',
   "Salima",
@@ -32,3 +37,5 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
+
+*/
