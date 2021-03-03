20db5062a5116cbe80844a637a6e74535f9938f4
mwkho <martin.h@hotmail.ca>
2021-03-03 23:40:25 +0000

    fixed ternerary station to if...else

--- letterPositions.js
+++ letterPositions.js
@@ -1,11 +1,50 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) { //for unequal lengths array
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  let equality =  '!==';
+  let status = 'Failed';
+  if (actual === expected) {
+    equality = '===';
+    status = 'Passed';
+  }
+  console.log(`Assertion ${status}: ${actual} ${equality} ${expected}.`);
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  let status = '!==';
+  let equality = 'Failed';
+  if (eqArrays(arrayOne, arrayTwo)) {
+    status = '===';
+    equality = 'Pass';
+  }
+  console.log(`Assertion ${equality}: [${arrayOne}] ${status} [${arrayTwo}]`);
+};
+
+// assertArraysEqual([true], [1]);
+
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++){
     let character = sentence[i];
     if (checkIsLetter(character)){
-      results[character] = results[character] ? results[character].push(i) : [i];
+      if (results[character]){
+        results[character].push(i);
+      } else{
+        results[character] = [i];
+      }
     }
   }
+  // console.log(results);
   return results;
 
 };
@@ -15,4 +54,13 @@ const checkIsLetter = function(letter) {
   abc += abc.toUpperCase();
   return (abc.indexOf(letter) !== -1);
 };
+
+let myStr = '"Lighthouse in the house"'
+let results = letterPositions(myStr);
+
+assertArraysEqual(results["h"], [4,6,16,19]);
+assertEqual(results["!"], undefined);
+assertEqual(results['"'], undefined);
+assertArraysEqual(results["L"], [1]);
+
 
