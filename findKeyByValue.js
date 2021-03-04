90c1bb847d3cecd92467ba31a92897a116f1fd48
mwkho <martin.h@hotmail.ca>
2021-03-04 01:10:40 +0000

    completed the findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  let equality =  '!==';
+  let status = 'Failed';
+  if (actual === expected) {
+    equality = '===';
+    status = 'Passed';
+  }
+  console.log(`Assertion ${status}: ${actual} ${equality} ${expected}.`);
+};
+
+const findKeyByValue = function(myObject, val){
+  let key;
+  for (let k of Object.keys(myObject)){
+    if (myObject[k] === val){
+      key = k;
+      break;
+    }
+  }
+  return key;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
