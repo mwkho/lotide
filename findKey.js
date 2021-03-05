088f8f77824d46d208d5fda73adbc608438b56ff
mwkho <martin.h@hotmail.ca>
2021-03-05 09:39:11 +0000

    linted findKey.js

--- findKey.js
+++ findKey.js
@@ -8,21 +8,21 @@ const assertEqual = function(actual, expected) {
   console.log(`Assertion ${status}: ${actual} ${equality} ${expected}.`);
 };
 
-const findKey = function(object1, callback){
+const findKey = function(object1, callback) {
   let found;
   let keys = Object.keys(object1);
   
 
   // assuming for each key, the value is another object,
   // can filter out the pertinent information
-  for (let key of keys){
-    if (callback(object1[key])){
+  for (let key of keys) {
+    if (callback(object1[key])) {
       found = key;
       break;
     }
   }
   return found;
-}
+};
 
 let test = findKey({
   "Blue Hill": { stars: 1 },
@@ -31,7 +31,7 @@ let test = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2); // => "noma"
 
 
 assertEqual(test, 'noma');
