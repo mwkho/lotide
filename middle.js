2d644197b784896cbebdd91b9a44ba2cacd4d59a
mwkho <martin.h@hotmail.ca>
2021-03-08 19:16:46 +0000

    refactored middle.js to not include copied code of testing

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) { //for unequal lengths array
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  let status = '!==';
-  let equality = 'Failed';
-  if (eqArrays(arrayOne, arrayTwo)) {
-    status = '===';
-    equality = 'Pass';
-  }
-  console.log(`Assertion ${equality}: [${arrayOne}] ${status} [${arrayTwo}]`);
-};
-
-// assertArraysEqual([true], [1]);
-
 const middle = function(arr) {
   let middleArray = []; // initialize an empty array
   let middleMark;
@@ -37,14 +13,6 @@ const middle = function(arr) {
   return middleArray;
 };
 
-
-/*
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1,2]),[]);
-assertArraysEqual(middle([]),[]);
-*/
-
+module.exports = middle;
 
 
