a5d191a2231d77136b26535231ebcd02afcdabe4
mwkho <martin.h@hotmail.ca>
2021-03-08 17:57:28 +0000

    moved the test cases into the appropriate file

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -3,3 +3,4 @@ const assertEqual = require("../assertEqual");
 assertEqual(1,1);
 assertEqual(1,'1');
 assertEqual(1,2);
+assertEqual('hello', 'hello');
--- test/headTest.js
+++ test/headTest.js
@@ -4,3 +4,7 @@ const assertEqual = require("../assertEqual");
 
 assertEqual([1,2,3], 1);
 assertEqual([1,2,3], 2);
+assertEqual([], undefined);
+assertEqual([1], 1);
+
+
